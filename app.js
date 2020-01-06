48cc41d480e24dd4fce73a53b2eb842cd127d745
Don <donch@chouinard.io>
2020-01-05 16:08:55 -0800

    Added JavaScript stuff.

---
+++ app.js
@@ -0,0 +1,16 @@
+//var let const
+// var is globally scoped, so don't use it any more
+//let and const wee added in ES2015
+//With let, you can re-assignvalues
+
+//String, Numbers, Boolean, null, undefined, symbol
+
+const name ='Curly';
+const age = 4.5;
+const isCool = true;
+const x = null;
+const y = undefined;
+let z;
+
+console.log(typeof age);
+console.log(`My name is ${name} and I am ${age}.`);
